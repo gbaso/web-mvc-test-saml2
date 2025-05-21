435110449df95ebe92f45effb11fc1a6880a0a33
Giacomo Baso <gbaso@users.noreply.github.com>
2025-05-21 12:00:44 +0200

    add application

---
+++ src/main/java/com/github/gbaso/webmvctestsaml2/WebMvcTestSaml2Application.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.webmvctestsaml2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcTestSaml2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebMvcTestSaml2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/gbaso/webmvctestsaml2/WebMvcTestSaml2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.webmvctestsaml2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcTestSaml2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
